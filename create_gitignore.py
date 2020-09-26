1b756b3170b809b42ba7764c2701999bce68c10e
Elena <kolomeets.elena7@gmail.com>
2020-09-26 12:49:51 +0200

    Allow multiple args to --add; add error message if path does not exist; wrap args parsing in main()

--- create_gitignore.py
+++ create_gitignore.py
@@ -1,37 +1,46 @@
 import argparse
 import os
-
-# os.getcwd() - current working directory
-# os.path.abspath('.') - absolute path to cwd
-# os.popen(filename, 'w'/'r') - open the file to write/read
-# os.popen(filename)
-# os.path.exists, os.path.isdir
+import sys
 
 ignore_files = ['.DS_Store\n', '.idea\n', '.pytest_cache/\n', '__pycache__/\n', '*.spec\n', 'build/\n', 'dist/\n',
                 'venv/\n']
 
 
 def create(path, ignore=None):
-    with open("{}\\.gitignore".format(path), mode="a+", encoding="utf-8") as file:
-        for i in ignore_files:
-            file.seek(0)
-            if i not in file.read():
-                file.write(i)
-        if ignore:
-            if ignore not in file.read():
-                file.write(ignore+'\n')
+    if os.path.exists(path):
+        with open("{}\\.gitignore".format(path), mode="a+", encoding="utf-8") as file:
+            for i in ignore_files:
+                file.seek(0)
+                if i not in file.read():
+                    file.write(i)
+            if ignore:
+                print(ignore)
+                for j in ignore:
+                    file.seek(0)
+                    if j+'\n' not in file.read():
+                        file.write(j+'\n')
+    else:
+        print("Error: The specified path does not exist", file=sys.stdout)
 
 
-parser = argparse.ArgumentParser(prog=".gitignore creator",
-                                 description="Creates a .gitignore file with basic content: .DS_Store, .idea, "
-                                             ".pytest_cache/, __pycache__/, *.spec, build/, dist/, venv/",
-                                 usage="Run without arguments to create .gitignore file with basic contents "
-                                       "in the current directory or provide the path name and additional content to "
-                                       "create/update .gitignore",
-                                 add_help=True
-                                 )
-parser.add_argument('--path', '-p', type=str, help="To create .gitignore outside of current directory provide the path",
-                    default=str(os.getcwd()))
-parser.add_argument('--add', '-a', type=str, help="To add one file/folder to .gitignore provide its name")
-args = parser.parse_args()
-create(args.path, args.add)
+def main():
+    parser = argparse.ArgumentParser(prog=".gitignore creator",
+                                         description="Creates a .gitignore file with basic content: .DS_Store, .idea, "
+                                                     ".pytest_cache/, __pycache__/, *.spec, build/, dist/, venv/; "
+                                                     "additional files can be added and the path can be specified.",
+                                         usage="Run without arguments to create .gitignore file with basic contents "
+                                               "in the current directory or provide the path name and additional "
+                                               "content to create/update .gitignore",
+                                         add_help=True
+                                         )
+    parser.add_argument('--path', '-p', type=str, help="To create .gitignore outside of the current directory provide "
+                                                       "the path",
+                            default=str(os.getcwd()))
+    parser.add_argument('--add', '-a', type=str, nargs='*', help="To add one or multiple files/folders to .gitignore "
+                                                                 "provide their names (separate with space)")
+    args = parser.parse_args()
+    create(args.path, args.add)
+
+
+if __name__ == '__main__':
+    main()
